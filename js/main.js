013f5ef0dede2af86a371ee39afe315695a23599
pamblam <adelphia@protonmail.ch>
2020-07-21 12:55:35 -0400

    moved assets to separate folders and made language toggleable via ajax

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+
+let toBeTranslated = document.querySelector('#translator');
+let toBeCopied = document.querySelector('#buffer');
+let langButton = document.querySelector('#lang__button');
+let helpButton = document.querySelector('#help');
+let closeButton = document.querySelector('#popup__close');
+let popUp = document.querySelector('#popup');
+var mapObj; // we'll assign later
+
+loadPage();
+
+var tr = () => {
+	//To grab contents use .value not .innerHTML
+	let content = toBeTranslated.value.toUpperCase();
+	content = content.replace(/[A-Z]/gi, (matched) => `${mapObj[matched]} `);
+	toBeCopied.innerHTML = content;
+}
+
+var copy = () => {
+	toBeCopied.select();
+	document.execCommand('copy');
+	//same here
+	document.getElementById('input__button-copy').value = 'Copied!';
+	setInterval(() => {
+		document.getElementById('input__button-copy').value = 'Copy';
+	}, 3000);
+}
+
+helpButton.onclick = () => {
+	//Just trying out different things. You can actually break the layout here but I'm gonna leave it.
+	document.querySelector('#container').classList.toggle('blur');
+	popUp.style.visibility = 'visible';
+}
+
+closeButton.onclick = () => {
+	document.querySelector('#container').classList.toggle('blur');
+	popUp.style.visibility = 'hidden';
+}
+
+langButton.addEventListener('click', function(e) {
+	e.preventDefault();
+	var language = this.innerText === 'RU' ? 'Russian' : 'English';
+	loadPage(language);
+});
+
+function loadPage(language="English"){
+	fetch("strings/languages.json")
+		.then(json=>json.json())
+		.then(lang=>{
+			mapObj = lang[language].mapObj;
+			Object.keys(lang[language]).forEach(key=>{
+				if(document.getElementById(key)){
+					document.getElementById(key).innerText = lang[language][key];
+				}
+			});
+		});
+}
